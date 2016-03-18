5707eb596611aa6e2c52890d5664550484e89994
小韬hao <774177563@qq.com>
2016-03-18 18:36:25 +0800

    delete

--- src/components/common-header/index.js
+++
@@ -1,9 +0,0 @@
-require('./style.styl')
-
-module.exports = {
-	template: require('./template.html'),
-	data:function () {
-    return { test: 123 }
-  },
-	props: ['title','leftLabel','rightLabel']
-} 
