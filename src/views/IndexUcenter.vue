4d9dbe119f3acb99d321b44dcfa6f63c82ef3656
wym <wym@wymdeMacBook-Pro.local>
2016-03-23 13:30:48 +0800

    add ucenter

--- src/views/IndexUcenter.vue
+++ src/views/IndexUcenter.vue
@@ -1,5 +1,28 @@
 <template>
       <flex-scroll-view>
+        <header class="uc-header">
+          <div>
+            <img src="../asset/images/user-white.png">
+            <p>18782950298</p>
+          </div>
+        </header>
+        <section class="li-section">
+          <div class="li-label"><a href="">
+              <div class="icon">123</div>
+              <div class="title">我的订单</div>
+              <div clase="arrow">123</div>
+            </a></div>
+          <div><a href="">
+              <div></div>
+              <div></div>
+              <div></div>
+            </a></div>
+          <div><a href="">
+              <div></div>
+              <div></div>
+              <div></div>
+            </a></div>
+        </section>
           <!-- <app-pane side="left" msg="1123" name="{{leftName}}"></app-pane> -->
       </flex-scroll-view>
 </template>
@@ -19,4 +42,29 @@
 
 <style lang="stylus">
   @import "../main.styl"
+  .uc-header
+    background:url(../asset/images/news.png)
+    text-align:center
+    padding-top:1rem
+    padding-bottom:.6rem
+    color:white
+  .li-section .li-label
+    width:100%
+    display:-webkit-box
+    display: -webkit-box; /* Safari */
+    & a
+      width:100%
+      display:-webkit-box
+      display: -webkit-box; /* Safari */
+      & .title
+        -webkit-box-flex: 1
+        flex:1
+      & .icon
+        width:.5rem
+        height:.5rem
+      & .arrow
+        width:.5rem
+        height:.5rem
+
+
 </style>
